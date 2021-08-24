3849e384d5f0e98f90f62ee3a37ab8d2206bf9e8
Patryk Młodzianowski <patryk.mlodzianowski@wp.pl>
2021-08-24 07:54:50 +0200

    Init commit

---
+++ WeatherClassification.ML.Model/ModelInput.cs
@@ -0,0 +1,16 @@
+using Microsoft.ML.Data;
+
+namespace WeatherClassification.ML.Model
+{
+    public class ModelInput
+    {
+        [ColumnName("Label"), LoadColumn(0)]
+        public string Label { get; set; }
+
+
+        [ColumnName("ImageSource"), LoadColumn(1)]
+        public string ImageSource { get; set; }
+
+
+    }
+}
---
+++ WeatherClassification.ML/DataPreparation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace WeatherClassification.ML
+{
+    public static class DataPreparation
+    {
+        public static (string trainDatasetPath, string testDatasetPath) GenerateDataSetFile(string dataPath, uint percentOfTotalImagesForTests = 15)
+        {
+            var dataSetTrainFilePath = Path.Combine(dataPath, "dataset-train.tsv");
+            var dataSetTestFilePath = Path.Combine(dataPath, "dataset-test.tsv");
+
+            using (var dataTrainFile = File.CreateText(dataSetTrainFilePath))
+            using (var dataTestFile = File.CreateText(dataSetTestFilePath))
+            {
+                dataTrainFile.WriteLine("Label\tImageSource");
+                dataTestFile.WriteLine("Label\tImageSource");
+
+                var dirs = Directory.GetDirectories(dataPath);
+                foreach (var dir in dirs)
+                {
+                    var files = Directory.GetFiles(dir);
+                    var testSetTreshold = Math.Ceiling(files.Length * percentOfTotalImagesForTests / 100d);
+
+                    for (int i = 0; i < files.Length; ++i)
+                    {
+                        var label = new DirectoryInfo(dir).Name;
+                        var file = files[i];
+                        var line = $"{label}\t{file}";
+
+                        if (i < testSetTreshold)
+                            dataTestFile.WriteLine(line);
+                        else
+                            dataTrainFile.WriteLine(line);
+                    }
+                }
+            }
+
+            return (dataSetTrainFilePath, dataSetTestFilePath);
+        }
+    }
+}
